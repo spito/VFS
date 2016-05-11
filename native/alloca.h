5890ff271e8e54c7e0e398294ea974706148ebef
Vladimir Still <vl.still@gmail.com>
2016-05-11 15:25:21 +0200

    Add support for calling some native syscalls using code from MUSL.

---
+++ native/alloca.h
@@ -0,0 +1,23 @@
+// Taken from MUSL library, edited by Vladimir Still
+
+#ifndef	_ALLOCA_H
+#define	_ALLOCA_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define	__NEED_size_t
+#include <bits/alltypes.h>
+
+void *alloca(size_t);
+
+#ifdef __GNUC__
+#define alloca __builtin_alloca
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
