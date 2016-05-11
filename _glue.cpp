90a0a4110f55e44d4edfa3bf6b20c251af633073
Vladimir Still <vl.still@gmail.com>
2016-05-11 15:28:28 +0200

    New native userspace implementation and stubs.

--- _glue.cpp
+++
@@ -1,9 +0,0 @@
-extern "C" {
-extern void abort();
-void __divine_problem( int, const char * ) noexcept {}
-void __divine_interrupt_mask() noexcept {}
-void __divine_interrupt_unmask() noexcept {}
-void __divine_assert( int value ) noexcept {
-    if ( !value ) abort();
-}
-}
